e94b55a8f47491ac703140dca96e4a2f02e9b3a0
rkoopmans <me@remcokoopmans.com>
2019-04-25 00:06:42 +0200

    Add --info cli flag for printing chain information instead of pem's

--- cert_chain_resolver/cli.py
+++ cert_chain_resolver/cli.py
@@ -7,6 +7,7 @@ def cli():
     parser = argparse.ArgumentParser(description='Ssl certificate chain resolver')
     parser.add_argument('certificate', nargs='?', default=sys.stdin, type=open, help='file formatted as PEM')
     parser.add_argument('-d', '--depth', nargs='?', default=None, type=int, help='Recursion max-depth. Default is until no parent cert is found')
+    parser.add_argument('-i', '--info', action='store_true', help='Print chain derived information')
 
     if sys.stdin.isatty() and len(sys.argv) == 1:
         sys.argv += ['-h']
@@ -16,7 +17,11 @@ def cli():
     cr = ChainResolver(depth=args.depth)
     try:
         cr.resolve(cert)
-        sys.stdout.writelines([x.export() for x in cr.list()])
+        if args.info:
+            import pprint
+            pprint.pprint([x.details for x in cr.list()], indent=2)
+        else:
+            sys.stdout.writelines([x.export() for x in cr.list()])
     except UnsuportedCertificateType as e:
         sys.stderr.write(repr(e) + '\n')
 
