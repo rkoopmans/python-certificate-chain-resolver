e3535ca9219a48535e696b98fc82c64de58f36fd
rkoopmans <me@remcokoopmans.com>
2020-12-06 01:17:06 +0100

    Run 'black' formatter for code consistency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt', 'r') as fh:
+with open("requirements.txt", "r") as fh:
     reqs = fh.readlines()
 
 setuptools.setup(
@@ -11,17 +11,13 @@ setuptools.setup(
     version="0.2.2",
     author="Remco Koopmans",
     author_email="me@remcokoopmans.com",
-    description="Resolve certificate chain by giving a cert as input",
+    description="Resolve / obtain the certificate intermediates of a x509 cert",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rkoopmans/python-certificate-chain-resolver",
     packages=setuptools.find_packages(),
     install_requires=reqs,
-    entry_points={
-        'console_scripts': [
-            "cert-chain-resolver = cli:cli"
-        ]
-    },
+    entry_points={"console_scripts": ["cert-chain-resolver = cli:cli"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2.7",
