a495a6eb6797937003225dc8072ecd176da07163
rkoopmans <me@remcokoopmans.com>
2020-12-11 00:48:58 +0100

    Add resolver tests

---
+++ tests/test_resolver.py
@@ -0,0 +1,21 @@
+from cert_chain_resolver.resolver import resolve
+from cert_chain_resolver.models import Cert
+
+
+def test_resolve_works_recursively(monkeypatch, mocker):
+    leaf = mocker.Mock()
+    intermediate = mocker.Mock()
+    ca = mocker.Mock()
+    ca.ca_issuer_access_location = None
+
+    resolve_mock = mocker.MagicMock()
+    resolve_mock.side_effect = [leaf, intermediate, ca]
+    monkeypatch.setattr("cert_chain_resolver.resolver.load_bytes_to_x509", resolve_mock)
+
+    monkeypatch.setattr(
+        "cert_chain_resolver.resolver._download",
+        mocker.Mock(side_effect=["intermediate", "ca"]),
+    )
+
+    chain = resolve(b"hoi")
+    assert list(chain) == [leaf, intermediate, ca]
