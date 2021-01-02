7d889d6023920ffcad0b3ef4dea67b14aea7cb6e
rkoopmans <me@remcokoopmans.com>
2021-01-03 00:54:01 +0100

    Add --info flag for printing details of each cert in chain

--- cert_chain_resolver/cli.py
+++ cert_chain_resolver/cli.py
@@ -4,16 +4,37 @@ from cert_chain_resolver.resolver import resolve
 from cert_chain_resolver import __is_py3__
 
 
-def cli(source=None, depth=None, info=None):
-    certs = resolve(source)
-    if info:
-        import pprint
+def _print_chain_details(chain):
+    for index, cert in enumerate(chain, 1):
+        print("== Certificate #{0} ==".format(index))
+        print("Subject:".ljust(20) + cert.subject)
+        print("Issuer:".ljust(20) + cert.issuer)
+        print("NotBefore:".ljust(20) + cert.not_valid_before.isoformat())
+        print("NotAfter:".ljust(20) + cert.not_valid_after.isoformat())
+        print("Serial:".ljust(20) + str(cert.serial))
+        print("Sha256Fingeprint:".ljust(20) + str(cert.fingerprint))
+        print("CAIssuerLoc:".ljust(20) + cert.ca_issuer_access_location if cert.ca_issuer_access_location else "")
+        print("Is root:".ljust(20) + repr(cert.is_root))
+        print("Is CA:".ljust(20) + repr(cert.is_ca))
+        print("Domains:")
+        print("  Common name:".ljust(20) + cert.common_name)
+        if cert.subject_alternative_names:
+            print("  SANExtensions:")
+            for domain in cert.subject_alternative_names:
+                print("    " + domain)
+        print("")
 
-        pprint.pprint([x.details for x in cr.list()], indent=2)
+
+def cli(file_bytes=None, show_details=None):
+    chain = resolve(file_bytes)
+    if show_details:
+        _print_chain_details(chain)
     else:
-        for c in [certs.leaf] + list(certs.intermediates):
+        for c in [chain.leaf] + list(chain.intermediates):
             sys.stdout.write(c.export())
 
+        for i, c in enumerate([chain.leaf] + list(chain.intermediates), 1):
+            sys.stderr.write(str(i) + ". " + repr(c) + "\n")
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -33,12 +54,7 @@ Examples:
     """,
     )
     parser.add_argument(
-        "file_name",
-        nargs="?",
-        default="-",
-        dest="source",
-        type=str,
-        help="file formatted as PEM",
+        "file_name", nargs="?", default="-", type=str, help="file formatted as PEM",
     )
     parser.add_argument(
         "-i", "--info", action="store_true", help="Print chain derived information"
@@ -50,17 +66,22 @@ if __name__ == "__main__":
     if sys.stdin.isatty() and len(sys.argv) == 1:
         sys.argv += ["-h"]
 
-    pargs = parse_args()
-    args = vars(pargs)
+    args = parse_args()
 
-    if args["source"] == "-":
+    cli_args = {
+        "file_bytes": None,
+        "show_details": args.info,
+    }
+
+    if args.file_name == "-":
+        source = None
         if __is_py3__:
             source = sys.stdin.buffer
         else:
             source = sys.stdin
-        args["source"] = source.read()
+        cli_args["file_bytes"] = source.read()
     else:
-        with open(args["source"], "rb") as f:
-            args["source"] = f.read()
+        with open(args.file_name, "rb") as f:
+            cli_args["file_bytes"] = f.read()
 
-    cli(**args)
+    cli(**cli_args)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,8 +12,50 @@ except NameError:
 
 @pytest.mark.parametrize("bundle", BUNDLE_FIXTURES, ids=certfixture_to_id)
 def test_cert_returns_completed_chain(capsys, bundle):
-    cli(source=bundle[0]["cert_pem"])
+    cli(file_bytes=bundle[0]["cert_pem"])
 
     captured = unicode(capsys.readouterr().out)
     expected = "".join([unicode(x["cert_pem"], "ascii") for x in bundle])
     assert captured == expected
+
+
+def test_display_flag_is_properly_formatted(capsys):
+    bundle = BUNDLE_FIXTURES[0]
+    cli(file_bytes=bundle[0]["cert_pem"], show_details=True)
+
+    captured = unicode(capsys.readouterr().out)
+
+    expected = unicode(
+        """== Certificate #1 ==
+Subject:            CN=github.com,O=GitHub\\, Inc.,L=San Francisco,ST=California,C=US
+Issuer:             CN=DigiCert SHA2 High Assurance Server CA,OU=www.digicert.com,O=DigiCert Inc,C=US
+NotBefore:          2020-05-05T00:00:00
+NotAfter:           2022-05-10T12:00:00
+Serial:             7101927171473588541993819712332065657
+Sha256Fingeprint:   b6b9a6af3e866cbe0e6a307e7dda173b372b2d3ac3f06af15f97718773848008
+CAIssuerLoc:        http://cacerts.digicert.com/DigiCertSHA2HighAssuranceServerCA.crt
+Is root:            False
+Is CA:              False
+Domains:
+  Common name:      github.com
+  SANExtensions:
+    github.com
+    www.github.com
+
+== Certificate #2 ==
+Subject:            CN=DigiCert SHA2 High Assurance Server CA,OU=www.digicert.com,O=DigiCert Inc,C=US
+Issuer:             CN=DigiCert High Assurance EV Root CA,OU=www.digicert.com,O=DigiCert Inc,C=US
+NotBefore:          2013-10-22T12:00:00
+NotAfter:           2028-10-22T12:00:00
+Serial:             6489877074546166222510380951761917343
+Sha256Fingeprint:   19400be5b7a31fb733917700789d2f0a2471c0c9d506c0e504c06c16d7cb17c0
+
+Is root:            False
+Is CA:              True
+Domains:
+  Common name:      DigiCert SHA2 High Assurance Server CA
+
+"""
+    )
+
+    assert expected == captured
